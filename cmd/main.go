04a88f83cf94c81401013ba6b33a76a941aa543c
Ahmet DEMİROK <ahmetbto42@gmail.com>
2022-09-03 22:18:43 +0300

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("index Page"))
+
+}
