2897689cf6debc68a515e4dcc759a899fa266158
Ahmet DEMİROK <ahmetbto42@gmail.com>
2022-09-04 15:50:05 +0300

    id select

--- cmd/main.go
+++ cmd/main.go
@@ -3,16 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	fmt.Println("Hello World")
-	http.HandleFunc("/", index)
-	http.ListenAndServe(":8080", nil)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", index)
+	r.HandleFunc("/post/{id}", Post)
+
+	http.ListenAndServe(":8080", r)
 
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("index Page."))
+
+}
+
+func Post(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Println(vars["id"])
+
+	w.Write([]byte("Post Page."))
 
 }
